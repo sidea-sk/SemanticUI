9e9dab12a05f1fb53b81bfb7a790be0d671df467
mics <michal.strbka@gmail.com>
2020-07-08 10:12:03 +0200

    Menu

---
+++ Source/SemanticUI.Blazor/Menus/MenuItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Sidea.SemanticUI.Core;
+
+namespace Sidea.SemanticUI.Blazor.Menus
+{
+    public abstract class MenuItem : SemanticComponent
+    {
+        public MenuItem() : base("item", prefixUiClass: false)
+        {
+        }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public Color Color { get; set; }
+
+        [Parameter]
+        public bool IsActive { get; set; }
+
+        [Parameter]
+        public string RightIcon { get; set; }
+
+        protected bool HasRightIcon => !string.IsNullOrWhiteSpace(this.RightIcon);
+
+        protected sealed override IEnumerable<string> Classes()
+        {
+            yield return this.Color.ToClass();
+            yield return "active".ToClassIf(this.IsActive);
+        }
+    }
+}
---
+++ Source/SemanticUI.Core/Common/ActiveUrlMatch.cs
@@ -0,0 +1,8 @@
+﻿namespace Sidea.SemanticUI.Core
+{
+    public enum ActiveUrlMatch
+    {
+        None,
+        Exact
+    }
+}
