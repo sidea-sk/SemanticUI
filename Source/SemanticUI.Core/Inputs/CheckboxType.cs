9ccd4667f27c3ecb78f9b0a08e1cb4f07a00a7ef
mics <michal.strbka@gmail.com>
2020-07-08 14:40:40 +0200

    Checkbox

---
+++ Source/SemanticUI.Core/Inputs/CheckboxType.cs
@@ -0,0 +1,18 @@
+﻿namespace Sidea.SemanticUI.Core
+{
+    public enum CheckboxType
+    {
+        Checkbox,
+        Radio,
+        Slider,
+        Toggle
+    }
+
+    public static class CheckboxTypeExtensions
+    {
+        public static string ToClass(this CheckboxType type)
+        {
+            return type.ToString().ToLower();
+        }
+    }
+}
