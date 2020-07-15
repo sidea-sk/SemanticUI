4678c66eea867e3ff88283e88d7d7968d9b347b6
mics <michal.strbka@gmail.com>
2020-07-15 19:28:02 +0200

    Embedded css files in the Core library

---
+++ Source/SemanticUI.Core/css/FileProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace Sidea.SemanticUI.Core
+{
+    public class FileProvider : IFileProvider
+    {
+        private const string _assemblyPath = "css/semantic-ui";
+        private ManifestEmbeddedFileProvider _provider = new ManifestEmbeddedFileProvider(typeof(FileProvider).Assembly);
+
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            var result = _provider.GetDirectoryContents(_assemblyPath + subpath);
+            return result;
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            var result = _provider.GetFileInfo(_assemblyPath + subpath);
+            return result;
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            var result = _provider.Watch(_assemblyPath + filter);
+            return result;
+        }
+    }
+}
---
+++ Source/SemanticUI.Core/css/ResourceLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Sidea.SemanticUI.Core.css
+{
+    public static class ResourceLoader
+    {
+        private static Assembly _assemlby = typeof(ResourceLoader).Assembly;
+
+        public static string Load(string resourceFilePath)
+        {
+            var resourceName = resourceFilePath.Replace("/", ".");
+            using Stream stream = _assemlby.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream);
+
+            string result = reader.ReadToEnd();
+            return result;
+        }
+    }
+}
