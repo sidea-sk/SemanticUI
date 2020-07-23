ba9f8bcb5310171adf9bc3c4a315b537a7b20b3a
mics <michal.strbka@gmail.com>
2020-07-23 15:46:47 +0200

    Forms

---
+++ Source/SemanticUI.TagHelpers/Forms/Fields.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Sidea.SemanticUI.Core;
+
+namespace Sidea.SemanticUI.TagHelpers.Forms
+{
+    [HtmlTargetElement("sui-fields")]
+    public class Fields : SemanticTagHelper
+    {
+        public Fields() : base("fields", "div")
+        {
+        }
+
+        public Amount Count { get; set; }
+
+        public bool Inline { get; set; }
+
+        protected override IEnumerable<string> Classes()
+        {
+            yield return this.Count.ToClass(string.Empty);
+            yield return "inline".ToClassIf(this.Inline);
+        }
+    }
+}
