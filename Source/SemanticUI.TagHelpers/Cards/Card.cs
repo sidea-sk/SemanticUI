6beabe4d201eb4e9380c2a668e45f2876796a265
mics <michal.strbka@gmail.com>
2020-07-27 15:09:15 +0200

    Cards

---
+++ Source/SemanticUI.TagHelpers/Cards/Card.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Sidea.SemanticUI.Core;
+
+namespace Sidea.SemanticUI.TagHelpers.Cards
+{
+    [HtmlTargetElement("sui-card")]
+    public class Card : SemanticTagHelper
+    {
+        public Card() : base("card", "div")
+        {
+        }
+
+        public Color Color { get; set; }
+
+        public CardDecorations Decorations { get; set; }
+
+        protected override IEnumerable<string> Classes()
+        {
+            yield return this.Color.ToClass();
+            yield return this.Decorations.ToClass();
+        }
+    }
+}
---
+++ Source/SemanticUI.TagHelpers/Cards/CardMeta.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sidea.SemanticUI.TagHelpers.Cards
+{
+    [HtmlTargetElement("sui-card-meta")]
+    public class CardMeta : SemanticTagHelper
+    {
+        public CardMeta() : base("meta", "div", false)
+        {
+        }
+
+        protected override IEnumerable<string> Classes()
+        {
+            yield break;
+        }
+    }
+}
