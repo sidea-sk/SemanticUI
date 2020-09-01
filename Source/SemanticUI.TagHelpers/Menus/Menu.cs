f8d6eb529f980d90bea9bb5a0a85c9eac5893209
mics <michal.strbka@gmail.com>
2020-09-01 16:26:43 +0200

    updates

--- Source/SemanticUI.TagHelpers/Menus/Menu.cs
+++ Source/SemanticUI.TagHelpers/Menus/Menu.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Sidea.SemanticUI.Core;
 
 namespace Sidea.SemanticUI.TagHelpers
 {
@@ -7,6 +10,13 @@ namespace Sidea.SemanticUI.TagHelpers
     {
         public Menu() : base(true)
         {
+        }
+
+        public Size Size { get; set; }
+
+        protected override IEnumerable<string> Classes()
+        {
+            return base.Classes().Concat(new[] { this.Size.ToClass() } );
         }
     }
 }
