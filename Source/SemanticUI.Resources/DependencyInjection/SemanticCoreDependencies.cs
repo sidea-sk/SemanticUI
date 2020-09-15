62822c15c6fe2359a9733e883742f97845a0d404
Michal Strbka <michal.strbka@gmail.com>
2020-09-15 10:58:40 +0200

    Resources Builder

---
+++ Source/SemanticUI.Resources/DependencyInjection/SemanticCoreDependencies.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.AspNetCore.Builder
+{
+    public static class SemanticCoreDependencies
+    {
+        /// <summary>
+        /// Enables semantuic.ui resources (css and js)
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="requestPath">
+        /// the request path must correspond with <link rel="stylesheet" href="/{requestPath}/semantic.min.css" />
+        /// e.g. requestPath = '/semantic-ui-folder'
+        /// and the link <link rel="stylesheet" href="/semantic-ui-folder/semantic.min.css" />
+        /// </param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseSemanticUiResources(this IApplicationBuilder app, string requestPath = "/semantic-ui")
+        {
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                RequestPath = "/sidea-core/semantic-ui",
+                FileProvider = new Sidea.SemanticUI.Resources.ResourceFileProvider()
+            });
+
+            return app;
+        }
+    }
+}
--- Source/SemanticUI.TagHelpers.Sample/Startup.cs
+++ Source/SemanticUI.TagHelpers.Sample/Startup.cs
@@ -24,12 +24,8 @@ namespace SemanticUI.TagHelpers.Sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSemanticUiResources("/sidea-core/semantic-ui");
             app.UseStaticFiles();
-            app.UseStaticFiles(new StaticFileOptions
-            {
-                RequestPath = "/sidea-core/semantic-ui", // the request path must correspond with <link rel="stylesheet" href="/sidea-core/semantic-ui/semantic.min.css" />
-                FileProvider = new Sidea.SemanticUI.Resources.ResourceFileProvider()
-            });
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
